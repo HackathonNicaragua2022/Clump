f8642403ec3b474871ffb79a13bd8bfe225432df
Alexandra Gutierrez <alexandrag1272@gmail.com>
2022-11-11 17:13:20 -0600

    Login y registro terminado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 import Tarjeta from "./components/Tarjeta";
+import Login from "./components/Login";
+import Registro from "./components/Registro";
 
 function App() {
   return (
     <div className="App">
-      <Tarjeta
-        titulo={"Hello world"}
-        img={{
-          src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-          alt: "arbolito",
-        }}
-      />
+      <Login />
     </div>
   );
 }
