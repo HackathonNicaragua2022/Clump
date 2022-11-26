fe256c5cdc08b6dc31a52f7a9e7516cbe373e8ef
Alexandra Gutierrez <alexandrag1272@gmail.com>
2022-11-26 09:59:39 -0600

    Nav-corregido

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,48 +7,39 @@ import useWindowDimensions from "../../hooks/WindowDimensions";
 import { useState } from 'react';
 
 const Navbar = () => {
-    let classnav = {
-        navbar_logo: 'img-logo',
-        navbar_menu: 'nav-toggle',
-        navbar_name: 'branding',
-        navbar_list: 'classul'
-    }
 
+    const [open, setOpen] = useState(false);
+    const [classnav, setClassnav] = useState("nav-toggle");
+    const [classlist, setClasslist] = useState("classul");
 
-    const { windowWidth } = useWindowDimensions();
-    if (windowWidth < 900) {
-        classnav.navbar_menu = 'nav-active';
-        classnav.navbar_list = 'classul-close';
-        classnav.navbar_logo = 'classul-close';
-        classnav.navbar_name = 'classul-close';
-    }
-    const changeclass = () => {
-        if (classnav.navbar_menu === 'nav-active') {
-            console.log('active');
-            classnav.navbar_menu = 'nav-toggle';
+    const handleClick = () => {
+        setOpen(!open);
+        if (open) {
+            setClassnav("nav-toggle");
+            setClasslist("classul-close");
+        } else {
+            setClassnav("nav-active");
+            setClasslist("classul");
+
         }
-        else if (classnav.navbar_menu === 'nav-toggle') {
-            console.log(classnav.navbar_list);
-            classnav.navbar_menu = 'nav-active';
-            classnav.navbar_list = 'classul';
-        }
-    }
+        console.log(open)
+    };
     return <nav>
         <div className="logo">
-            <img src={Logo} className={classnav.navbar_logo} alt='Logo' />
+            <img src={Logo} className='img-logo' alt='Logo' />
         </div>
-        <button className={classnav.navbar_menu} aria-label="Abrir menú" onClick={changeclass}>
+        <button className={classnav} aria-label="Abrir menú" onClick={handleClick}>
             <i className="fas fa-bars"></i>
         </button>
-        <div className={classnav.navbar_logo}>
+        <div className='img-logo'>
             <p className="brand-name">
                 Class Clump
             </p>
         </div>
-        <ul className={classnav.navbar_list}>
-            <Link to='/'><li>Inicio</li></Link>
-            <Link to='/register'><li>Registrarse</li></Link>
-            <Link to='/login'><li>Iniciar Sesión</li></Link>
+        <ul className={classlist}>
+            <Link to='/' className='list-link'><li className='lisk-item'>Inicio</li></Link>
+            <Link to='/register' className='list-link'><li className='lisk-item'>Registrarse</li></Link>
+            <Link to='/login' className='list-link'><li className='lisk-item'>Iniciar Sesión</li></Link>
         </ul>
     </nav >;
 };
